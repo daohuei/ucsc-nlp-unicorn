5af8ad17645819945d9ee56c3fb724ddc82e2d27
Chih-Kai Ting <tingken0214@gmail.com>
2022-05-09 16:12:47 -0700

    feat(nlp-203): hw2 - add data preparation script

---
+++ nlp_203/hw2/prepare_data_no_bpe.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Adapted from https://github.com/facebookresearch/MIXER/blob/master/prepareData.sh
+
+# echo 'Cloning Moses github repository (for tokenization scripts)...'
+# git clone https://github.com/moses-smt/mosesdecoder.git
+
+# echo 'Cloning Subword NMT repository (for BPE pre-processing)...'
+# git clone https://github.com/rsennrich/subword-nmt.git
+
+SCRIPTS=lib/mosesdecoder/scripts
+TOKENIZER=$SCRIPTS/tokenizer/tokenizer.perl
+CLEAN=$SCRIPTS/training/clean-corpus-n.perl
+NORM_PUNC=$SCRIPTS/tokenizer/normalize-punctuation.perl
+REM_NON_PRINT_CHAR=$SCRIPTS/tokenizer/remove-non-printing-char.perl
+BPEROOT=lib/subword-nmt/subword_nmt
+BPE_TOKENS=40000
+
+CORPORA=(
+    "fr-en/train.tags.fr-en"
+)
+
+if [ ! -d "$SCRIPTS" ]; then
+    echo "Please set SCRIPTS variable correctly to point to Moses scripts."
+    exit
+fi
+
+src=fr
+tgt=en
+lang=fr-en
+prep=iwslt13_fr_en_no_bpe
+tmp=$prep/tmp
+orig=data
+
+mkdir -p $orig $tmp $prep
+
+echo "pre-processing train data..."
+for l in $src $tgt; do
+    rm $tmp/train.$l
+    for f in "${CORPORA[@]}"; do
+        cat $orig/$f.$l |
+            perl $NORM_PUNC $l |
+            perl $REM_NON_PRINT_CHAR |
+            perl $TOKENIZER -threads 8 -a -l $l >>$tmp/train.$l
+    done
+done
+
+echo "pre-processing dev data..."
+for l in $src $tgt; do
+    cat $orig/iwslt13.dev.$l |
+        perl $TOKENIZER -threads 8 -a -l $l >$tmp/dev.$l
+    echo ""
+done
+
+echo "pre-processing test data..."
+for l in $src $tgt; do
+    cat $orig/iwslt13.test.$l |
+        perl $TOKENIZER -threads 8 -a -l $l >$tmp/test.$l
+    echo ""
+done
+
+TRAIN=$tmp/train.fr-en
+BPE_CODE=$prep/code
+rm -f $TRAIN
+for l in $src $tgt; do
+    cat $tmp/train.$l >>$TRAIN
+done
+
+for L in $src $tgt; do
+    for f in train.$L dev.$L test.$L; do
+        cp $tmp/$f $prep/$f
+    done
+done
