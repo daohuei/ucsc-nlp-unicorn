c21a0c251ac81cd3c6748b08d3ecaf7cdd150c84
Chih-Kai Ting <tingken0214@gmail.com>
2022-05-20 00:33:08 -0700

    feat(nlp-244): hw2 - add few more prompts

--- nlp_244/hw2/config.py
+++ nlp_244/hw2/config.py
@@ -7,7 +7,7 @@ PROMPTS = [
     ("", ", my emotion is [MASK]."),
     ("I feel frustrated, my emotion is sadness.", ", my emotion is [MASK].",),
     (
-        "I feel frustrated, my emotion is sadness. I feel happy, my emotion is joy.  I feel surprised, my emotion is surprise. I feel scared, my emotion is fear. ",
+        "I feel frustrated, my emotion is sadness. I feel happy, my emotion is joy. ",
         ", my emotion is [MASK].",
     ),
     (
@@ -15,8 +15,13 @@ PROMPTS = [
         ", my emotion is [MASK].",
     ),
     ("", ". I feel [MASK]."),
+    ("", ". emotion is [MASK]."),
+    ("", " It has the emotion of [MASK]."),
+    ("", "It feels [MASK]."),
+    ("", " It is [MASK]."),
+    ("", " It has the feeling of [MASK]."),
 ]
-PROMPT_CHOICE = 4
+PROMPT_CHOICE = 5
 
 NAME = f"prompt_{PROMPT_CHOICE}"
 PRE_PROMPT = PROMPTS[PROMPT_CHOICE][0]
